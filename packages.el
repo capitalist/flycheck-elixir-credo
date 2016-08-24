da11e1b5bcaa84779a571fdd6f378c75bba9619e
Joe Martinez <jrm02t@gmail.com>
2016-08-24 15:43:02 -0400

    Fixes namespacing in package.el

--- packages.el
+++ packages.el
@@ -1,12 +1,12 @@
-(setq elixir-credo-packages
+(setq flycheck-elixir-credo-packages
       '(flycheck
-        (flycheck-elixir-credo :location (recipe :fetcher github :repo "smeevil/flycheck-elixir-credo"))
+        (flycheck-elixir-credo :location (recipe :fetcher github :repo "capitalist/flycheck-elixir-credo"))
          ))
 
-(defun elixir-credo/init-flycheck-elixir-credo ()
+(defun flycheck-elixir-credo/init-flycheck-elixir-credo ()
   (use-package flycheck-elixir-credo
                :defer t
                :init (add-hook 'elixir-mode-hook 'flycheck-elixir-credo-setup)))
 
-(defun elixir-credo/post-init-flycheck ()
+(defun flycheck-elixir-credo/post-init-flycheck ()
   (add-hook 'elixir-mode-hook 'flycheck-mode))
